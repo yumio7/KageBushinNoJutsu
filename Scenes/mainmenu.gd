9044d00db2276a5edff958a5689449d36482d676
Connor <csaila@live.com>
2024-10-14 13:39:26 -0400

    quit button works (the third one)

--- Scenes/mainmenu.gd
+++ Scenes/mainmenu.gd
@@ -7,3 +7,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
