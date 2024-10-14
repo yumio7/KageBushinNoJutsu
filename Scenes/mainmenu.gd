cee8fbf46bad2016958abee076371a83a330b4bb
Connor <csaila@live.com>
2024-10-14 12:58:13 -0400

    you can select the buttons with kb or gamepad

--- Scenes/mainmenu.gd
+++ Scenes/mainmenu.gd
@@ -3,8 +3,6 @@ extends MarginContainer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$HBoxContainer/VBoxContainer/MenuButtons/play.grab_focus()
-	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
