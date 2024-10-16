9a9491fc89e70980faca7533f5ff2a2303373fd8
Connor <csaila@live.com>
2024-10-16 19:24:47 -0400

    copy over minamitsu script

--- Scenes/Entities/minamitsu.gd
+++ Scenes/Entities/minamitsu.gd
@@ -1,25 +1,47 @@
 extends CharacterBody2D
 
+const walkSpeed: float = 50.0			# Base walking Movement speed
+const walkSpeedMax: float = 200.0		# Maximum walking movement speed. Minamitsu cannot move faster than this when walking.
+const jumpVelocity: float = -250.0		# Jump velocity. It is negative because in Godot up is negative y.
+const fallSpeedMax: float = 250.0		# Maximum fall velocity. Minamitsu cannot fall faster than this.
+const dashVelocity: float = 300.0		# Dash velocity.
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+# Tracks state of character for different movement behaviours
+enum state {Idle = 0, Walk = 1, Jump = 2, Fall = 3, Ability = 4}	# Define enum for every state
+var currentState: int = state.Idle									# Tracks the current state. By default, it is set to idle.
+var walkDirection: int = 0												# Keep track of whether controls for walking are being held down
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+func _physics_process(delta: float) -> void:	
+	# Apply gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if Input.is_action_pressed("Ability") and (currentState == state.Fall || currentState == state.Jump):
+			velocity.y = jumpVelocity
+			currentState = state.Ability
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("Jump") and is_on_floor():
+		velocity.y = jumpVelocity
+	
+	# double jump
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction := Input.get_axis("Left", "Right") # left is neg, right is pos, neutral is zero
+	if direction and abs(velocity.x) < walkSpeedMax:
+		velocity.x += direction * walkSpeed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, walkSpeed)
+
+	if direction > 0:
+		$AnimatedSprite2D.flip_h = false;
+	elif direction < 0: 
+		$AnimatedSprite2D.flip_h = true;
+
+	if is_on_floor() and velocity.x == 0: currentState = state.Idle
+	elif is_on_floor() and velocity.x != 0: currentState = state.Walk
+	elif !is_on_floor() and currentState == state.Ability: pass
+	elif !is_on_floor() and velocity.y >= 0: currentState = state.Fall
+	elif !is_on_floor() and velocity.y < 0: currentState = state.Jump
 
 	move_and_slide()
+	$AnimatedSprite2D.play("Idle")
