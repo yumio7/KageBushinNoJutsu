77f65da294909ae33049c94bee3f83ced1c7b8f4
Connor <csaila@live.com>
2024-10-19 14:47:02 -0400

    minamitsu real!!!!

--- Scenes/Entities/minamitsu.gd
+++ Scenes/Entities/minamitsu.gd
@@ -1,47 +1,118 @@
 extends CharacterBody2D
+# TODO (If possible)
+# Coyote time
+# Jump buffer
+# Variant jump height depending on how long the jump input is held
 
+# Constants. Assigned values cannot be changed
 const walkSpeed: float = 50.0			# Base walking Movement speed
-const walkSpeedMax: float = 200.0		# Maximum walking movement speed. Minamitsu cannot move faster than this when walking.
-const jumpVelocity: float = -250.0		# Jump velocity. It is negative because in Godot up is negative y.
-const fallSpeedMax: float = 250.0		# Maximum fall velocity. Minamitsu cannot fall faster than this.
-const dashVelocity: float = 300.0		# Dash velocity.
+const walkSpeedMax: float = 125.0		# Maximum walking movement speed. Minamitsu cannot move faster than this when walking.
+const jumpVelocity: float = -200.0		# Jump velocity. It is negative because in Godot up is negative y.
+const fallSpeedMax: float = 200.0		# Maximum fall velocity. Minamitsu cannot fall faster than this.
+const anchorVelocity: float = 250.0		# Velocity at which the anchor flies through the air
+const grappleVelocity: float = 250.0	# Velocity at which Minamitsu is pulled to the anchor
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 # Tracks state of character for different movement behaviours
 enum state {Idle = 0, Walk = 1, Jump = 2, Fall = 3, Ability = 4}	# Define enum for every state
 var currentState: int = state.Idle									# Tracks the current state. By default, it is set to idle.
-var walkDirection: int = 0												# Keep track of whether controls for walking are being held down
+var walkDirection = 0												# Keep track of whether controls for walking are being held down
+var previousFloorState = false										# Tracks the previous grounded state of player. Used for Coyote Time
 
-func _physics_process(delta: float) -> void:	
-	# Apply gravity
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		if Input.is_action_pressed("Ability") and (currentState == state.Fall || currentState == state.Jump):
-			velocity.y = jumpVelocity
-			currentState = state.Ability
+# Runs once when the character is instantiated
+func _ready() -> void:
+	pass # Nothing happens here for now
 
-	# Handle jump.
-	if Input.is_action_pressed("Jump") and is_on_floor():
-		velocity.y = jumpVelocity
+# Runs every physics frame (60fps)
+func _physics_process(delta: float) -> void:
 	
-	# double jump
+	#print("0-" + str(currentState)) # FOR DEBUGGING: Print the current state before all physics logic executes
 
-	var direction := Input.get_axis("Left", "Right") # left is neg, right is pos, neutral is zero
-	if direction and abs(velocity.x) < walkSpeedMax:
-		velocity.x += direction * walkSpeed
-	else:
-		velocity.x = move_toward(velocity.x, 0, walkSpeed)
+	# Dash ability. Sets the state to ability and sets velocity
+	if Input.is_action_just_pressed("Ability") and $Timers/AnchorCooldown.time_left <= 0:
+		pass
+	# If the user is using an ability, lock all other movement
+	if currentState != state.Ability:
+		# Check if player is holding down movement control. Move them if yes.
+		walkDirection = Input.get_axis("Left", "Right")
+		# Track if user is making walking input and moves character in that direction.
+		if walkDirection and abs(velocity.x) < walkSpeedMax:
+			velocity.x += walkDirection * walkSpeed
+		else:
+			velocity.x = move_toward(velocity.x, 0, walkSpeed)
+		
+		# Determine if the player can jump with this variable
+		var canJump = false
+		# Determine if coyote time is allowed by comparing previous floor state with current ground state
+		if !is_on_floor() and previousFloorState == true: $Timers/JumpCoyoteTime.start()
+		previousFloorState = is_on_floor() # Update the previous floor state after check
 
-	if direction > 0:
-		$AnimatedSprite2D.flip_h = false;
-	elif direction < 0: 
-		$AnimatedSprite2D.flip_h = true;
+		# Check if player is inputting jump. Depending on how long they input, their jump height will change
+		if Input.is_action_just_pressed("Jump") and is_on_floor(): canJump = true
+		# Jump buffer timer starts when player jumps but is not on floor
+		elif Input.is_action_just_pressed("Jump") and !is_on_floor() and $Timers/JumpCoyoteTime.time_left == 0: $Timers/JumpBuffer.start()
+		# If jump buffer timer is on and player lands, they jump
+		elif Input.is_action_pressed("Jump") and is_on_floor() and $Timers/JumpBuffer.time_left > 0: canJump = true
+		# If coyote time is on and player jumps, they jump
+		elif Input.is_action_just_pressed("Jump") and !is_on_floor() and $Timers/JumpCoyoteTime.time_left > 0: canJump = true
+		# Depending on length of input, jump height increases
+		elif Input.is_action_pressed("Jump") and !is_on_floor() and $Timers/JumpHeightTimer.time_left > 0: velocity.y = jumpVelocity
+		# Edge case in case the player releases&inputs again when JumpHeightTimer is on.
+		elif Input.is_action_just_released("Jump"): $Timers/JumpHeightTimer.stop()
 
-	if is_on_floor() and velocity.x == 0: currentState = state.Idle
-	elif is_on_floor() and velocity.x != 0: currentState = state.Walk
-	elif !is_on_floor() and currentState == state.Ability: pass
-	elif !is_on_floor() and velocity.y >= 0: currentState = state.Fall
-	elif !is_on_floor() and velocity.y < 0: currentState = state.Jump
+		if canJump == true:
+			$Timers/JumpHeightTimer.start()
+			velocity.y = jumpVelocity
 
+		# Dictate state depending on the physics of the character.
+		if is_on_floor() and velocity.x == 0:
+			currentState = state.Idle
+		elif is_on_floor() and velocity.x != 0: currentState = state.Walk
+		elif !is_on_floor() and velocity.y >= 0: currentState = state.Fall
+		elif !is_on_floor() and velocity.y < 0: currentState = state.Jump
+
+	# State machine. Depending on the currentState, run a different function with different movement behaviour
+	match currentState:
+		0: stateIdle()
+		1: stateWalk()
+		2: stateJump(delta)
+		3: stateFall(delta)
+		4: stateAbility()
+
+	#print("1-" + str(currentState)) # FOR DEBUGGING: Print the current state after all physics logic executes
+
+	# Flip sprite depending on horizontal velocity
+	if walkDirection > 0:
+		$AnimatedSprite2D.flip_h = false
+	elif walkDirection < 0:
+		$AnimatedSprite2D.flip_h = true
+
+	# Character will slide along surfaces when moving
 	move_and_slide()
+
+# Specific behaviours for each state
+func stateIdle():
 	$AnimatedSprite2D.play("Idle")
+	
+func stateWalk():
+	$AnimatedSprite2D.play("Walk")
+
+func stateJump(delta):
+	$AnimatedSprite2D.play("Jump")
+	applyGravity(delta) # Gravity
+
+func stateFall(delta):
+	$AnimatedSprite2D.play("Fall")
+	applyGravity(delta) # Gravity
+	
+func stateAbility():
+	$AnimatedSprite2D.play("Dash")
+
+# Function to apply gravity
+func applyGravity(delta):
+	# Apply gravity to vertical velocity until max falling speed reached
+	if not is_on_floor() and velocity.y < fallSpeedMax:
+		velocity.y += gravity * delta
+
+func _on_dash_duration_timeout():
+	currentState = state.Idle
