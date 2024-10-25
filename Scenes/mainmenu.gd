07ee0993c9d33dfae110e802935c9e3d93253b17
Connor <csaila@live.com>
2024-10-24 21:19:16 -0400

    menu buttons & intro flow

--- Scenes/mainmenu.gd
+++ Scenes/mainmenu.gd
@@ -2,14 +2,10 @@ extends MarginContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$HBoxContainer/VBoxContainer/MenuButtons/play.grab_focus()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	$HBoxContainer/MenuButtons/play.grab_focus()
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/testlevel.tscn")
+	get_tree().change_scene_to_file("res://Scenes/RootScenes/intro.tscn")
 	# TODO: play should boot to level select
 
 func _on_settings_pressed() -> void:
