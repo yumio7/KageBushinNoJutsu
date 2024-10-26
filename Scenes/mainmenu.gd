126a011d1b421d5346ccb79d8e3477c1fe7afc31
Connor <csaila@live.com>
2024-10-26 19:29:04 -0400

    main menu background

--- Scenes/mainmenu.gd
+++ Scenes/mainmenu.gd
@@ -1,8 +1,8 @@
-extends MarginContainer
+extends TextureRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$HBoxContainer/MenuButtons/play.grab_focus()
+	$HBoxContainer/MenuButtons/play.call_deferred("grab_focus")
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/RootScenes/intro.tscn")
