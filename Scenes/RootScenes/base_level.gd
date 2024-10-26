ef27103f97ecae26ce88e96f83ad44abe15c746f
Quarterpot <149041343+Quarterpot@users.noreply.github.com>
2024-10-26 17:27:43 -0400

    Added music and small changes to lvl 1
    Bruhrburbrubrurhurbuh hina cooked.
    Did some balancing for level one

--- Scenes/RootScenes/base_level.gd
+++ Scenes/RootScenes/base_level.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 @export var initDialoguePause: bool = true	# Dictates whether the character should be paused for initial dialogue
 var initialControlledCharacter: CharacterBody2D
+var musicLoop: AudioStreamPlayer = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,10 +12,17 @@ func _ready() -> void:
 	if initialControlledCharacter != null and initDialoguePause:
 		initialControlledCharacter.currentState = 5 # Set the state to pause
 
+	# Initialize vertical mirror normal. Prevents edge cases when transitioning level of the worldboundary normal not being reset when stage changed
 	var verticalMirror = $Entities.find_child("VerticalMirror")
 	if verticalMirror != null:
 		verticalMirror.find_child("CollisionShape2D").shape.normal = Vector2(-1, 0)
+	
+	# Get the music loop if it exists
+	musicLoop = $Music.find_child("MusicLoop")
 
 # When dialogue has ended, unpause the character
 func _on_dialogue_end_dialogue() -> void:
 	initialControlledCharacter.currentState = 0
+
+	if musicLoop != null:
+		musicLoop.play()
